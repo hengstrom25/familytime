071c308597be5989fac8fb2350d52b9391b8861d
Heidi <hengstrom25@gmail.com>
2019-10-06 16:46:48 -0400

    migrate users table, create sessions and static pages controllers

---
+++ familytime/app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+require 'pry'
+
+    def new
+      @user = User.new
+    end
+ 
+    def create
+  		@user = User.find_by(name: params[:user][:name])
+  		if @user && @user.authenticate(params[:user][:password])
+  			login
+  		else
+  			flash[:alert] = "Please make sure you have logged in correctly. If you do not have a login, please create a new account."
+  			redirect_to signin_path
+  			end
+  		end
+    end
+    
+    def destroy
+      session.clear
+      redirect_to root_path
+    end
+  
+  private
+  
+  def login
+  	session[:user_id] = @user.id
+  	redirect_to user_path(@user)
+  end
+ 
+
+end
