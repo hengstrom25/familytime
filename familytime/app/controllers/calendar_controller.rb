bd1d9f0ef44d0a21d5db4fdec6337984e930ba50
Heidi <hengstrom25@gmail.com>
2019-10-27 16:22:03 -0400

    new calendar controller

--- familytime/app/controllers/calendar_controller.rb
+++ familytime/app/controllers/calendar_controller.rb
@@ -1,4 +1,19 @@
 class CalendarsController < ApplicationController
 
+	def new
+		@calendar = Calendar.new
+	end
+	
+	def create
+		@calendar = Calendar.new(calendar_params)
+		@calendar.save
+		render json: calendar, status: 201
+	end
+	
+	private
+		
+	def calendar_params
+  		params.require(:calendar).permit(:title)
+  	end
 
 end
